c961e5d1c25b7ea54b913f6ff1748b8e92991ae6
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-27 22:18:33 +0700

    Refactor

--- main.py
+++ main.py
@@ -13,22 +13,22 @@ def download_images():
         response = requests.get(image_url)  
         if response.status_code == 200:  
             
-            with open(f"raw_images/{i}.jpg", "wb") as file:  # Open a file for each image
+            filename = f"raw_images/{i:03d}.jpg"  # Construct the file name for each image with 3 decimals
+
+            with open(filename, "wb") as file:  # Open a file for each image
                 file.write(response.content)  # Write the content of the response to the file
-            print(f"Downloaded: {i}.")
+            print(f"Downloaded: {filename}")
         else:
             print(f"Failed: {i}.jpg -- {response.status_code} {response.reason}")
 
 def convert_images_to_pdf(image_dir, output_filename):
-    """
-    Converts all images in a directory to a single PDF file.
 
-    Args:
-    image_dir: Path to the directory containing images.
-    output_filename: Name of the output PDF file.
-    """
-    
+    pdf_path = f"{image_dir}/{output_filename}.pdf"
+    if os.path.exists(pdf_path):
+        return
+
     images = [Image.open(join(image_dir, f)) for f in listdir(image_dir) if isfile(join(image_dir, f)) and f.lower().split('.')[-1] in ('jpg', 'jpeg', 'png')]
+    
     # Check if any images were found
     if not images:
         print("No images found in directory")
@@ -36,16 +36,18 @@ def convert_images_to_pdf(image_dir, output_filename):
     
     first_image = images[0]
     # Create a new PDF with the same mode, size, and format as the first image
-    pdf_path = f"{image_dir}/{output_filename}.pdf"
     pdf = first_image.convert('RGB').save(pdf_path, save_all=True, append_images=images[1:])
 
     print(f"Successfully converted images to PDF: {pdf_path}")
 
 def main():
-    if not os.path.exists("./raw_images"):
-        os.makedirs("./raw_images")
+    image_dir = "raw_images"
+
+    if not os.path.exists(image_dir):
+        os.makedirs(image_dir)
         download_images()
-    convert_images_to_pdf("raw_images", "output")
+
+    convert_images_to_pdf(image_dir, "0_book")
 
 if __name__ == "__main__":
     main()
