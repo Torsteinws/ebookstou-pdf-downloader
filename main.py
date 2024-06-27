52c1d6ccc617b9d8c3d995489c57a5951de6b182
Torstein Wilhelmsen Sandvik <git@torsteinws.no>
2024-06-27 22:24:24 +0700

    More refactoring

--- main.py
+++ main.py
@@ -6,33 +6,35 @@ from PIL import Image
 
 def download_images(): 
     base_url = "http://readonline.ebookstou.org/flipbook/40908/files/mobile"
+    
     for i in range(1, 71):  # Iterate from 1 to 70
-        
-        image_url = f"{base_url}/{i}.jpg"  # Construct the URL for each image
-        
-        response = requests.get(image_url)  
-        if response.status_code == 200:  
-            
-            filename = f"raw_images/{i:03d}.jpg"  # Construct the file name for each image with 3 decimals
 
-            with open(filename, "wb") as file:  # Open a file for each image
-                file.write(response.content)  # Write the content of the response to the file
-            print(f"Downloaded: {filename}")
-        else:
-            print(f"Failed: {i}.jpg -- {response.status_code} {response.reason}")
+        # Send a GET request to the image URL
+        image_url = f"{base_url}/{i}.jpg"
+        response = requests.get(image_url)  
+
+        # Stop if the request was unsuccessful
+        if response.status_code != 200:
+            return print(f"Failed to download images: {response.status_code} {response.reason}")
+
+        # Save the image to a file
+        filename = f"raw_images/{i:03d}.jpg"
+        with open(filename, "wb") as file:
+            file.write(response.content)  
+        print(f"Downloaded: {filename}")
+
 
 def convert_images_to_pdf(image_dir, output_filename):
 
     pdf_path = f"{image_dir}/{output_filename}.pdf"
     if os.path.exists(pdf_path):
-        return
+        return print(f"PDF already exists: {pdf_path}")
 
     images = [Image.open(join(image_dir, f)) for f in listdir(image_dir) if isfile(join(image_dir, f)) and f.lower().split('.')[-1] in ('jpg', 'jpeg', 'png')]
     
     # Check if any images were found
     if not images:
-        print("No images found in directory")
-        return
+        return print("No images found in directory")
     
     first_image = images[0]
     # Create a new PDF with the same mode, size, and format as the first image
